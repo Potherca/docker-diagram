18a454481fb6e3a22aff661fa52cd050f33dcd25
Ben Peachey <ben@muze.nl>
2023-03-03 16:57:27 +0100

    Add basic javascript logic.

---
+++ web/app.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('#container')
+const form = document.querySelector('[data-form="json"]')
+const plantumlForm = document.querySelector('[data-form="plantuml"]')
+
+function parseJson(fromRepos, imageToRepoMap) {
+    return {
+        fromRepos,
+        imageToRepoMap
+    }
+}
+
+function parsePlantumlJson(plantumlJson) {
+    let plantuml = `@startuml\n`
+    plantuml += `@enduml\n`
+
+    return plantuml
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const fromRepos = JSON.parse(event.target.querySelector('[name="repo.json"]').value.trim())
+    const imageToRepoMap = JSON.parse(event.target.querySelector('[name="hub.json"]').value.trim())
+
+    const plantumlJson = parseJson(fromRepos, imageToRepoMap)
+
+    document.querySelector('.result').value = JSON.stringify(plantumlJson, null, 4)
+    plantumlForm.querySelector('button').attributes.removeNamedItem('disabled')
+})
+
+plantumlForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const blob = event.target.querySelector('textarea').value.trim()
+    const plantumlJson = JSON.parse(blob)
+
+    const plantuml = parsePlantumlJson(plantumlJson)
+
+    container.querySelector('textarea').value = plantuml
+})
